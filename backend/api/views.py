ca2c4a8021bf02c65d9bfbb296dcb32dbfe225ce
Deepak  Rajpurohit <deepak.dev@dreambox.cloud>
2020-11-10 21:54:25 +0530

    Initcommit

---
+++ backend/api/views.py
@@ -0,0 +1,14 @@
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
+from todos.models import Todo
+from .serializers import TodoSerializer
+
+
+class ListTodo(ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class DetailTodo(RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
